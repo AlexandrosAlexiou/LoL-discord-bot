c31e8cab128873ae9e256da14d65bc6f9e12cce1
AlexandrosAlexiou <alex.alexiouu@gmail.com>
2021-09-26 19:08:25 +0300

    Use different site for scraping

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 champions = set()
+roles = {'jungle', 'top', 'bot', 'support', 'mid'}
 # Strips the newline character
 for line in open('champions.csv', 'r').readlines():
     champions.add(line.split("\t")[0])
@@ -22,24 +23,26 @@ async def on_message(message):
     if message.author == client.user:  # we do not want the bot to reply to itself
         return
 
-    if message.content.startswith('!skills'):
+    if message.content.startswith('!build'):
         keywords = message.content.split(" ")
         champion = keywords[1]
         role = keywords[2]
 
         if champion not in champions:
-            await channel.send(f"Champion {champion} does not exist in the list -> " + champions)
+            await channel.send(f"Champion {champion} does not exist in the list -> " + str(list(champions)))
             return
 
-        scraper = Scraper(f"https://u.gg/lol/champions/{champion}/build?role={role}")
-        image = scraper.scrape(class_name="content-section_content.skill-path-block")
+        if role not in roles:
+            await channel.send(f"Role {role} does not exist in the list -> " + str(list(roles)))
+            return
+
+        scraper = Scraper(f"https://www.op.gg/champion/{champion}/statistics/{role}/build")
+        image = scraper.scrape(class_name="l-champion-statistics-content__main")
         image = Image.open(io.BytesIO(image))
 
         with io.BytesIO() as image_binary:
             image.save(image_binary, 'PNG')
             image_binary.seek(0)
             await channel.send(file=discord.File(fp=image_binary, filename='image.png'))
-
-
 
 client.run(TOKEN)
