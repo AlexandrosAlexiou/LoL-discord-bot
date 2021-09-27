e4bed826da6a766fcae0357e97f47cfa6ea82239
AlexandrosAlexiou <alex.alexiouu@gmail.com>
2021-09-27 14:29:59 +0300

    Accept GDPR pop-up of OP.GG
    The popup is interfering with the screenshot mechanism

--- scrape.py
+++ scrape.py
@@ -1,8 +1,10 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 
 class Scraper:
@@ -11,10 +13,11 @@ class Scraper:
         options.headless = True
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
-        self.driver = webdriver.Firefox(options=options)
+        self.driver = webdriver.Chrome(options=options)
         self.driver.get(page_url)
 
     def scrape(self, class_name=None, xpath=None):
+        WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CLASS_NAME, 'css-1litn2c'))).click()
         if class_name:
             element = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
         if xpath:
